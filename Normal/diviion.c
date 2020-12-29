f862427d7396ff3cefabf21d32203b258265d3bd
Raushan-PC\Administrator <raushanshrivastava69@gmail.com>
2020-12-29 10:59:24 -0800

    Created division program

---
+++ Normal/diviion.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+int x,y;
+printf ("Enter the value of x ");
+scanf("%d",&x);
+printf("Enter the value nof y");
+scanf("%d",&y);
+printf("The division of x and y is %d",x/y);
+return 0;}
