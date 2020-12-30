fec9b13114d5c0ba9c433fffde5f56fc409ea165
Raushan-PC\Administrator <raushanshrivastava69@gmail.com>
2020-12-29 18:57:00 -0800

    multiplication

---
+++ Normal/multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the value of x\n");
+    scanf("%d",&x);
+    printf("Enter the value of y\n");
+    scanf("%d",&y);
+    printf("The multiplication of x and y is %d",x*y);
+    return 0;
+}
